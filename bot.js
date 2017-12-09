8cc9664ac81fd2408f808bbaf2da48412606f46f
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2017-12-09 10:31:42 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,17 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'pidsadasdasdasdasdasdasdasd') {
+    msg.reply('Pong!');
+  }
+});
+
+
 /****************
  * IMPORTS
  */
@@ -120,3 +134,5 @@ SlackWelcomeBot.prototype.run = function () {
 
 var bot = new SlackWelcomeBot( process.env )
 bot.run()
+
+client.login = (process.env.BOT_TOKEN)
